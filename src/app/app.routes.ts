dad633f29ecf3660619b19a1922c735b0e06afe2
IvanPaz24 <ivanpazaguero@gmail.com>
2026-04-08 20:56:53 -0300

    ADD: tarea angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
+// import { Bienvenida } from './pages/bienvenida/bienvenida';
+// // import { Login } from './pages/login/login';
+// import { Registro } from './pages/registro/registro';
+// import { SobreMi } from './pages/sobre-mi/sobre-mi';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "bienvenida",
+        loadComponent: () => import('./pages/bienvenida/bienvenida').then((m) => m.Bienvenida ) 
+    },
+    {
+        path: "",
+        redirectTo: "/",
+        pathMatch: "full"
+    },
+    {
+        path: "login",
+        loadComponent: () => import('./pages/login/login').then((m) => m.Login ) 
+    },
+    {
+        path: "registro",
+         loadComponent: () => import('./pages/registro/registro').then((m) => m.Registro ) 
+    },
+    {
+        path: "sobre-mi",
+         loadComponent: () => import('./pages/sobre-mi/sobre-mi').then((m) => m.SobreMi ) 
+    },
+    { 
+        path: "error",
+        loadComponent: () => import('./pages/error/error').then((m) => m.Error )
+    },
+    {
+        path: "**",
+         loadComponent: () => import('./pages/error/error').then((m) => m.Error ) 
+    }
+];
